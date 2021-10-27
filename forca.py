846988b90d59f3644eedf5f057edf0b058e6f569
Giovanna Niemeyer <giovanna.assis@loggi.com>
2021-10-27 19:16:21 -0300

    the gamer is capable to see their hints

--- forca.py
+++ forca.py
@@ -3,15 +3,28 @@ def jogar():
     print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = 'banana'
+    palavra_jogo = []
 
-	enforcou = False
-	acertou = False
+    for x in palavra_secreta:
+        palavra_jogo.append('_')
 
-	while(not acertou and not enforcou):
-	    print("Jogando...")
+    print(palavra_jogo)
 
-    print("Fim do jogo")
+    enforcou = False
+    acertou = False
+
+    while(not acertou and not enforcou):
+        chute = input('Qual a letra? ').strip()
+
+        i: int = 0
+        for letra in palavra_secreta:
+            if chute.upper() == letra.upper():
+                palavra_jogo[i] = chute
+            i = i + 1
+        print(palavra_jogo)
+
+    print('Fim do jogo')
 
 
 if(__name__ == "__main__"):
