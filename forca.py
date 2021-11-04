51ccaf6c6e201a7cf5e93d3000d383df429f9166
Giovanna Niemeyer <giovanna.assis@loggi.com>
2021-11-04 11:56:07 -0300

    fixing while and if conditions

--- forca.py
+++ forca.py
@@ -13,17 +13,28 @@ def jogar():
 
     enforcou = False
     acertou = False
+    errors = 0
 
     while(not acertou and not enforcou):
         chute = input('Qual a letra? ').strip()
 
-        i: int = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                palavra_jogo[i] = chute
-            i = i + 1
-        print(palavra_jogo)
+        if chute in palavra_secreta:
+            i: int = 0
+            for letra in palavra_secreta:
+                if chute.upper() == letra.upper():
+                    palavra_jogo[i] = chute
+                i = i + 1
+        else:
+            errors = errors + 1
 
+        print(palavra_jogo)
+        enforcou = errors == 6
+        acertou = "_" not in palavra_jogo
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Quem sabe na próxima?")
     print('Fim do jogo')
 
 
